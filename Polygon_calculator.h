f9695c93cdfb63537d3fcd4f0afea5a5897a3700
UlianaBespalova <43138516+UlianaBespalova@users.noreply.github.com>
2019-12-25 20:23:04 +0300

    Update Polygon_calculator.h

--- Polygon_calculator.h
+++ Polygon_calculator.h
@@ -5,6 +5,16 @@ extern "C"
 #ifndef IV_POLYGON_CALCULATOR_H
 #define IV_POLYGON_CALCULATOR_H
 
+#ifdef __cplusplus
+extern "C"
+{double Get_value(char *str);
+
+void Coordinates_input (const int n, double *arr_x, double *arr_y);
+
+double Polygon_square(const int n, const double *arr_x, const double *arr_y);
+
+double Polygon_calculator();}
+#endif
 
 double Get_value(char *str);
 
@@ -13,7 +23,5 @@ void Coordinates_input (const int n, double *arr_x, double *arr_y);
 double Polygon_square(const int n, const double *arr_x, const double *arr_y);
 
 double Polygon_calculator();
-
-int Pol (int, int, int);
 
 #endif //IV_POLYGON_CALCULATOR_H
